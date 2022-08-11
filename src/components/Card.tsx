dd5574aafe1c53e08d373ceb8aff237e5d596c65
Diego Fernando Alayon <dfar.41@gmail.com>
2022-08-11 15:36:36 -0500

    instalando tailwindcss

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 interface Props {
   count: number,
-  children: React.ReactNode
+  // children: React.ReactNode
 }
 
 const Cart:React.FC<Props> = ({ count }) => {
   return (
     <>
-      <h1>Titulo</h1>
+      <h1 className='text-3xl font-bold text-red-200'>Titulo</h1>
       <h2>{count}</h2>
     </>
   )
