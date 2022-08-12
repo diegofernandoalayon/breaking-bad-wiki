5d14a2c2a3a8ac7948fbd584a20bb711cab760b8
Diego Fernando Alayon <dfar.41@gmail.com>
2022-08-11 20:07:14 -0500

    agregando react-router-dom v6

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,36 @@
 import { useState } from 'react'
+import { Route, Routes, Link } from 'react-router-dom'
 import './App.css'
 import Card from './components/Card'
 function App () {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-        <Card count={count} />
-        <button onClick={() => setCount(a => a + 1)}>sumar</button>
-    </div>
+    <>
+    <nav>
+      <ul>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='/characters'>Characters</Link>
+        </li>
+        <li>
+          <Link to='/episodes'>Episodes</Link>
+        </li>
+      </ul>
+    </nav>
+
+      <Routes >
+        <Route path ='/' element={<div className="App">
+          <Card count={count} />
+          <button onClick={() => setCount(a => a + 1)}>sumar</button>
+      </div>} />
+      <Route path='/characters' element={<h1>characters</h1>} />
+      <Route path='/episodes' element={<h1>episodios</h1>} />
+      </Routes>
+    </>
+
   )
 }
 
