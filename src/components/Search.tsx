507b45fb4ce671bde1870775f9f3db121caa8cd3
Diego Fernando Alayon <dfar.41@gmail.com>
2022-08-15 16:31:23 -0500

    creando componente Search

---
+++ src/components/Search.tsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+const Search = () => {
+  const [keyword, setKeyword] = useState<string>('')
+  const handleChange = (e:React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target
+    setKeyword(value)
+  }
+  const handleSubmit = (e:React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+  }
+  return (
+    <form onSubmit={handleSubmit} className='flex gap-1 justify-center m-2'>
+      <input type="text" placeholder='toto' autoFocus onChange={handleChange} value={keyword} className='w-full md:w-auto rounded px-2 py-2 text-lg'/>
+      <button className='bg-green-500 text-white px-2 hover:bg-green-400 rounded text-lg'>Buscar</button>
+    </form>
+  )
+}
+
+export default Search
