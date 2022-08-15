0e2aa220083f5954b771c5c46502795ffd5aea1a
Diego Fernando Alayon <dfar.41@gmail.com>
2022-08-15 16:30:09 -0500

    agregando funcionalidad a Home agregando efecto para pedir caracteres random por medio del servicio getRandomCharacters

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,24 @@
+import { useEffect, useState } from 'react'
+import getRandomCharacters from '../services/getRandomCharacters'
+import Search from '../components/Search'
+import { Character } from '../types'
+import ListCharacters from '../components/ListCharacters'
+
 function Home () {
+  const [randomCharacters, setRandomCharacters] = useState<Character[]>([])
+  useEffect(() => {
+    getRandomCharacters()
+      .then(res => {
+        setRandomCharacters(res)
+      })
+      .catch(err => console.error(err))
+  }, [])
   return (
-    <>
-    <h1 className='text-5xl'>Home</h1>
-    </>
+    <section className='my-2' >
+    <Search />
+    <h2 className='text-5xl'>Home</h2>
+    <ListCharacters characters={randomCharacters}/>
+    </section>
   )
 }
 
