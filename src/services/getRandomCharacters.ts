0347cf9f45759279699af08ae9f92218962465aa
Diego Fernando Alayon <dfar.41@gmail.com>
2022-08-15 16:28:30 -0500

    creando mapeo de datos

--- src/services/getRandomCharacters.ts
+++ src/services/getRandomCharacters.ts
@@ -1,8 +1,27 @@
 import axios from 'axios'
+import { Character, CharactersFromApi } from '../types'
 
 const BASE_URL = 'https://www.breakingbadapi.com/api'
+const mapApiToApp = (apiResponse: CharactersFromApi[]):Character[] => {
+  return apiResponse.map(characterFromApi => {
+    const {
+      nickname,
+      name,
+      char_id: id,
+      status,
+      img: image
+    } = characterFromApi
+    return {
+      name,
+      nickname,
+      id,
+      status,
+      image
+    }
+  })
+}
 const getRandomCharacters = async () => {
   const res = await axios.get(`${BASE_URL}/character/random?limit=10`)
-  return res.data
+  return mapApiToApp(res.data)
 }
 export default getRandomCharacters
