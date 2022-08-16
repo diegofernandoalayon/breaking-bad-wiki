da331008e870f03726fef904e660205204765dd1
Diego Fernando Alayon <dfar.41@gmail.com>
2022-08-16 15:03:45 -0500

    modificando estilos Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -19,20 +19,20 @@ function Header () {
   return (
     <header className='justify-between p-5 bg-white shadow md:flex md:items-center md:justify-between'>
       <div className='flex justify-between items-center'>
-        <h1 className='text-2xl font-[fantasy] cursor-pointer text-green-600 hover:opacity-80'>
-          breaking-bad-wiki
+        <h1 className='text-2xl font-mono font-bold cursor-pointer text-green-600 hover:text-green-500'>
+          <Link to='/'>breaking-bad-wiki</Link>
         </h1>
         <span onClick={handleBurger} className='text-3xl cursor-pointer mx-2 md:hidden'>
           {
             isMobile
-              ? <Close height={40} width={40} className='text-green-600 hover:opacity-80 '/>
-              : <Menu height={40} width={40} className='text-green-600 hover:opacity-80 '/>
+              ? <Close height={40} width={40} className='text-green-600 hover:text-green-500 '/>
+              : <Menu height={40} width={40} className='text-green-600 hover:text-green-500 '/>
           }
 
         </span>
       </div>
-      <nav>
-        <ul className={`md:flex md:items-center z-0 md:z-auto md:static absolute bg-white w-full left-0 md:w-auto md:py-0 py-4 md:pl-0 pl-7 md:opacity-100 duration-200 transition-all ease-in ${isMobile ? '' : 'opacity-0 top-[-400px]'}` }>
+      <nav className=' '>
+        <ul className={`md:flex md:items-center z-10 md:z-auto md:static absolute bg-white w-full left-0 md:w-auto md:py-0 py-4 md:pl-0 pl-7 md:opacity-100 duration-200 transition-all ease-in ${isMobile ? 'relative' : 'opacity-0 top-[-400px]'}` }>
           {
             ITEMS_NAV.map((e) => (
               <li key={e.link} className='mx-4 my-6 md:my-0'>
