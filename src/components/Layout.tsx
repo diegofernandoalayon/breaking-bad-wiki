ed769808cbbf2b2d76714251b55cd04a5db79c90
Diego Fernando Alayon <dfar.41@gmail.com>
2022-08-16 15:01:12 -0500

    agregando font-family agregando font-family al layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,7 +4,7 @@ interface Props {
 }
 const Layout = ({ children }:Props) => {
   return (
-  <div className='max-w-5xl mx-auto lg:w-5xl '>
+  <div className='max-w-5xl mx-auto lg:w-5xl font-sans '>
     {children}
   </div>
   )
