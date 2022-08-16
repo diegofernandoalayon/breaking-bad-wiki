6b7aab733440bdbe89b001bec1b05e077224fd12
Diego Fernando Alayon <dfar.41@gmail.com>
2022-08-16 15:02:11 -0500

    nueva request en Home agregando funcionalizada para pedir una nueva request random en la pagina home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,18 +6,22 @@ import ListCharacters from '../components/ListCharacters'
 
 function Home () {
   const [randomCharacters, setRandomCharacters] = useState<Character[]>([])
+  const [newRequest, setNewRequest] = useState<number>(0)
   useEffect(() => {
     getRandomCharacters()
       .then(res => {
         setRandomCharacters(res)
       })
       .catch(err => console.error(err))
-  }, [])
+  }, [newRequest])
   return (
     <section className='my-2' >
-    <Search />
-    <h2 className='text-5xl'>Home</h2>
-    <ListCharacters characters={randomCharacters}/>
+      <Search />
+      <div className='flex justify-around'>
+        <h2 className='text-5xl text-center'>Home</h2>
+        <button className='bg-green-500 text-white px-2 hover:bg-green-400 rounded text-lg' onClick={() => setNewRequest(r => r + 1)}>Random</button>
+      </div>
+      <ListCharacters characters={randomCharacters}/>
     </section>
   )
 }
