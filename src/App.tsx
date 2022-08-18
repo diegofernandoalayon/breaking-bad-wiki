d0fc506aebe7d955e8d03eb32c7fee2d05405077
Diego Fernando Alayon <dfar.41@gmail.com>
2022-08-18 12:00:16 -0500

    modificando estilos

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,17 @@ import './App.css'
 import Header from './components/Header'
 import Layout from './components/Layout'
 import Home from './pages/Home'
+import Characters from './pages/Characters'
 function App () {
   return (
-    <div className='bg-green-600 bg-opacity-20'>
-    <Layout>
+    <div className=' bg-green-500 bg-opacity-10 min-h-screen'>
+    <Layout >
 
     <Header />
 
       <Routes >
         <Route path ='/' element={<Home />}/>
-      <Route path='/characters' element={<h1>characters</h1>} />
+      <Route path='/characters' element={<Characters />} />
       <Route path='/episodes' element={<h1>episodios</h1>} />
       </Routes>
     </Layout>
