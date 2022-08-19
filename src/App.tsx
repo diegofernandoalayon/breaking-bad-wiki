fcde94860a629a339e1a444ff2a89660f9a481ab
Diego Fernando Alayon <dfar.41@gmail.com>
2022-08-19 14:43:52 -0500

    indicando provider del context

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,20 @@ import Header from './components/Header'
 import Layout from './components/Layout'
 import Home from './pages/Home'
 import Characters from './pages/Characters'
+import { CharactersContextProvider } from './context/CharactersContext'
 function App () {
   return (
     <div className=' bg-green-500 bg-opacity-10 min-h-screen'>
     <Layout >
 
     <Header />
-
+    <CharactersContextProvider>
       <Routes >
         <Route path ='/' element={<Home />}/>
-      <Route path='/characters' element={<Characters />} />
-      <Route path='/episodes' element={<h1>episodios</h1>} />
+        <Route path='/characters' element={<Characters />} />
+        <Route path='/episodes' element={<h1>episodios</h1>} />
       </Routes>
+    </CharactersContextProvider>
     </Layout>
     </div>
 
