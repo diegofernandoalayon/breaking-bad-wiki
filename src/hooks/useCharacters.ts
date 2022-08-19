f0a0450641782290872333f4c2f607a9ae29d7df
Diego Fernando Alayon <dfar.41@gmail.com>
2022-08-19 14:43:23 -0500

    usando el contexto Characters usando com useContext el contexto creado para los characters en el hook useCharacters

--- src/hooks/useCharacters.ts
+++ src/hooks/useCharacters.ts
@@ -1,12 +1,15 @@
 // react
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useContext } from 'react'
 // types
-import { Character } from '../types'
+// import { Character } from '../types'
 // services
 import getCharacters from '../services/getCharacters'
+// context
+import CharactersContext from '../context/CharactersContext'
 
 const useCharacters = () => {
-  const [characters, setCharacters] = useState<Character[]>([])
+  // const [characters, setCharacters] = useState<Character[]>([])
+  const { characters, setCharacters } = useContext(CharactersContext)
   const [loading, setLoading] = useState<boolean>(false)
   const [page, setPage] = useState<number>(0)
   useEffect(() => {
