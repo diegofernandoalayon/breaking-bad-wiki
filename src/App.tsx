c22363897857683cf6c368fd4bdd141bb5374b0d
Diego Fernando Alayon <dfar.41@gmail.com>
2022-08-22 16:08:33 -0500

    agregando ruta ruta para detallar characters y funcion de click para entrar al detalle de cada caracter

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Layout from './components/Layout'
 import Home from './pages/Home'
 import Characters from './pages/Characters'
 import { CharactersContextProvider } from './context/CharactersContext'
+import CharacterDetail from './pages/CharacterDetail'
 function App () {
   return (
     <div className=' bg-green-500 bg-opacity-10 min-h-screen'>
@@ -14,9 +15,10 @@ function App () {
     <Header />
     <CharactersContextProvider>
       <Routes >
-        <Route path ='/' element={<Home />}/>
-        <Route path='/characters' element={<Characters />} />
-        <Route path='/episodes' element={<h1>episodios</h1>} />
+        <Route path = '/' element={<Home />}/>
+        <Route path = '/characters' element={<Characters />} />
+        <Route path = '/characters/:id' element={<CharacterDetail/>} />
+        <Route path = '/episodes' element={<h1>episodios</h1>} />
       </Routes>
     </CharactersContextProvider>
     </Layout>
--- src/components/CharacterCard.tsx
+++ src/components/CharacterCard.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Character } from '../types'
+import { useNavigate } from 'react-router-dom'
 const alive = 'bg-green-300 text-green-800'
 const deceased = 'bg-red-300 text-red-800 '
 const other = 'bg-orange-300 text-orange-800'
 const CharacterCard:React.FC<Character> = ({ id, name, nickname, image, status }) => {
+  const navigate = useNavigate()
   return (
-    <div onClick={() => console.log(id)} className='w-[320px] h-2/6 relative mb-1'>
+    <div onClick={() => navigate(`/characters/${id}`)} className='w-[320px] h-2/6 relative mb-1'>
       <img src={image} alt={`profile ${name}`} className='w-full p-2 h-[400px] rounded-t-3xl object-cover'/>
 
       <div className='bg-white bg-opacity-75 ml-2 mr-2 absolute bottom-2 hover:-translate-y-1 w-[95%]'>
