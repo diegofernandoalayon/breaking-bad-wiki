e7cb9672f1cd49f4076e0de08d870992c33563c6
Diego Fernando Alayon <dfar.41@gmail.com>
2022-08-23 09:34:38 -0500

    mostrando detalle de character buscar en el context si el character existe, de lo contrario mandar peticion a la API

--- src/pages/CharacterDetail.tsx
+++ src/pages/CharacterDetail.tsx
@@ -1,33 +1,66 @@
-import { useContext } from 'react'
-import { useParams, Navigate } from 'react-router-dom'
+import { useContext, useEffect, useState } from 'react'
+import { useParams, useNavigate } from 'react-router-dom'
 import CharacterDetailCard from '../components/CharacterDetailCard'
 import CharactersContext from '../context/CharactersContext'
+import { ICharacterDetail } from '../types'
+import { getOneCharaterById } from '../services/getCharacters'
 
 function CharacterDetail () {
   const params = useParams()
+  const navigate = useNavigate()
+  const [oneCharacter, setOneCharacter] = useState<ICharacterDetail[]>()
   console.log(params)
   const { characters } = useContext(CharactersContext)
 
   const character = characters.find(c => c.id === +params.id!)
-  console.log(character)
-  if (character === undefined) {
-    console.log('tttoto')
-    return <Navigate to='/characters' />
-  }
+
+  useEffect(() => {
+    if (character === undefined) {
+      const { id } = params
+      if (id) {
+        getOneCharaterById({ id })
+          .then((res) => {
+            if (res.length === 0) {
+              throw new Error('not found character')
+            }
+            setOneCharacter(res)
+          })
+          .catch(() => {
+            navigate('/characters')
+          })
+      }
+    }
+  }, [])
   return (
     <div>
+      {
+        character &&
+          <CharacterDetailCard
+            id={character.id}
+            name={character.name}
+            nickname={character.birthday}
+            actor={character.actor}
+            birthday={character.birthday!}
+            image={character.image!}
+            occupation={character.occupation}
+            status={character.status}
+          />
 
-      <CharacterDetailCard
-        id={character.id}
-        name={character.name}
-        nickname={character.birthday}
-        actor={character.actor}
-        birthday={character.birthday!}
-        image={character.image!}
-        occupation={character.occupation}
-        status={character.status}
+      }
+      {
+        oneCharacter &&
+        <CharacterDetailCard
+          id={oneCharacter[0].id}
+          name={oneCharacter[0].name}
+          nickname={oneCharacter[0].birthday}
+          actor={oneCharacter[0].actor}
+          birthday={oneCharacter[0].birthday!}
+          image={oneCharacter[0].image!}
+          occupation={oneCharacter[0].occupation}
+          status={oneCharacter[0].status}
+        />
+      }
 
-      />
     </div>
   )
 }
