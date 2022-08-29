edcc49fd2506487445b947904742e40575c52ab9
Diego Fernando Alayon <dfar.41@gmail.com>
2022-08-28 20:54:56 -0500

    agregando funcionalidad Search

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -9,12 +9,23 @@ const Search = () => {
   }
   const handleSubmit = (e:React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
-    if (keyword) navigate(`/search/${keyword}`)
+    if (keyword) navigate(`/search/${keyword}`, { replace: true })
   }
   return (
     <form onSubmit={handleSubmit} className='flex gap-1 justify-center m-2'>
-      <input type="text" placeholder='toto' autoFocus onChange={handleChange} value={keyword} className='w-full md:w-auto rounded px-2 py-2 text-lg'/>
-      <button className='bg-green-500 text-white px-2 hover:bg-green-400 rounded text-lg'>Buscar</button>
+      <input
+        type="text"
+        placeholder='type character to search'
+        autoFocus
+        onChange={handleChange}
+        value={keyword}
+        className='w-full md:w-auto rounded px-2 py-2 text-lg'
+      />
+      <button
+        className='bg-green-500 text-white px-2 hover:bg-green-400 rounded text-lg'
+      >
+        Buscar
+      </button>
     </form>
   )
 }
