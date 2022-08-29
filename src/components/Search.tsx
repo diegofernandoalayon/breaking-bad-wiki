62a6393b8118dd798dff61d6738dc091d7a1e863
Diego Fernando Alayon <dfar.41@gmail.com>
2022-08-28 19:59:33 -0500

    agregando funcionalidad al Componente Search agregando funcion de navegar a una ruta con las opciones de la busqueda que desee el usuario

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,12 +1,15 @@
 import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 const Search = () => {
   const [keyword, setKeyword] = useState<string>('')
+  const navigate = useNavigate()
   const handleChange = (e:React.ChangeEvent<HTMLInputElement>) => {
     const { value } = e.target
     setKeyword(value)
   }
   const handleSubmit = (e:React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
+    if (keyword) navigate(`/search/${keyword}`)
   }
   return (
     <form onSubmit={handleSubmit} className='flex gap-1 justify-center m-2'>
