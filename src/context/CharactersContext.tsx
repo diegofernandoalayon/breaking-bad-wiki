f21041ac2723fba31ec0f2e41ebb9e03178b6e19
Diego Fernando Alayon <dfar.41@gmail.com>
2022-09-21 14:16:53 -0500

    usando useReducer estado characters usando useReducer para manejar el estado de charactersContext

--- src/context/CharactersContext.tsx
+++ src/context/CharactersContext.tsx
@@ -1,11 +1,14 @@
 
-import React, { useState, createContext } from 'react'
+import React, { useReducer, createContext } from 'react'
+import { charactersReducer, initialState } from '../reducers/charactersReducer'
 
 import { ICharacterDetail } from '../types'
 
 interface CharacterContextProps {
   characters: Array<ICharacterDetail>
-  setCharacters: React.Dispatch<React.SetStateAction<ICharacterDetail[]>>
+  // setCharacters: React.Dispatch<React.SetStateAction<ICharacterDetail[]>>
+  addCharacters: (res:ICharacterDetail[])=> void
+  updateCharacters: (res:ICharacterDetail[]) => void
 }
 
 export const CharactersContext = createContext<CharacterContextProps>({} as CharacterContextProps)
@@ -15,9 +18,13 @@ interface Props {
 }
 
 export const CharactersContextProvider = ({ children }:Props) => {
-  const [characters, setCharacters] = useState<ICharacterDetail[]>([])
+  // const [characters, setCharacters] = useState<ICharacterDetail[]>([])
+  const [characters, dispatch] = useReducer(charactersReducer, initialState)
+  const addCharacters = (res:ICharacterDetail[]) => dispatch({ type: 'add_characters', payload: res })
+  const updateCharacters = (res:ICharacterDetail[]) => dispatch({ type: 'update_characters', payload: res })
+  // const [page, setPage] = useState<number>(0)
   return (
-    <CharactersContext.Provider value={{ characters, setCharacters }} >
+    <CharactersContext.Provider value={{ characters, addCharacters, updateCharacters }} >
 
     {children}
     </CharactersContext.Provider>
